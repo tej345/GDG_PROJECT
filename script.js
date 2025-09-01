8bffb6027c010b830308e175906e08b43f60aec2
tej345 <tej345@users.noreply.github.com>
2025-09-01 19:58:21 +0530

    Render Tasks

--- script.js
+++ script.js
@@ -48,3 +48,32 @@ function addTask(text) {
 function saveTasks(){
     localStorage.setItem("tasks",JSON.stringify(tasks));
 }
+
+function renderTasks(){
+    const list = document.getElementById("task-list");
+    list.innerHTML = "";
+
+    if(tasks.length === 0){
+        list.innerHTML = `<p class = "text-gray-500 text-center mt-4">No tasks yet. Add one!</p>`;
+        return;
+    }
+
+    tasks.forEach(task => {
+        const item = document.createElement("div");
+        item.className = "flex items-center justify-between bg-white p-3 rounded-lg shadow mb-2 transition-all";
+        item.setAttribute("data-id",task.id);
+        item.innerHTML = `
+            <div class = "flex items-center">
+              <label class = "flex items-center cursor-pointer space-x-2">
+              <input type = "checkbox" class = "peer hidden" onchange = "toggleTaskComplete(${task.id})" ${task.completed ? "checked" : ""} />
+              <div class = "w-5 h-5 rounded-full border-2 border-purple-500 peer-checked:bg-purple-500 transition-all"></div>
+              <span class = " ${task.completed ? " line-through  text-gray-500" : ""}">${task.text}</span>
+              </label>
+              </div>
+              <div class = "space-x-2">
+                <button onclick = "editTask(${task.id})" class = " text-blue-500 hover:underline">Edit</button>
+                </div>
+                `;
+                list.appendChild(item);
+    });
+}
